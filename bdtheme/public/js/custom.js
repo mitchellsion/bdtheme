da39f6f3bb7ae385bd0c5a308dd2f0d514f92824
mitchell sion <mitchellsion02@gmail.com>
2018-10-31 10:05:33 +0000

    dynamic menu from doctype

--- bdtheme/public/js/custom.js
+++ bdtheme/public/js/custom.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	$('header').prepend(frappe.render_template("logo"));
 	$('header .navbar .container').prepend(frappe.render_template("sidebar-toggle"));
-	$('.main-section').append(frappe.render_template("main-sidebar"));
+    // $('.main-section').append(frappe.render_template("main-sidebar"));
+    
+    genereate_side_menu((ret)=>{
+        $('.main-section').append(ret);
+    })
+    
 
 	$('header').addClass('main-header');
 	$('header .navbar').removeClass('navbar-fixed-top');
@@ -18,8 +23,103 @@ frappe.provide("bdtheme");
 $(document).bind('toolbar_setup', function() {
 	frappe.app.name = "bdoop Erp";
 	$('.navbar-home').html(frappe._('Home'));
-
 });
+
+
+// generate menu dari doctype Sidebar Menu
+function genereate_side_menu(callback){
+    // Get sidebar header
+    var sidebarHeader = [];
+    frappe.call({
+		method:"frappe.client.get_list",
+		args: {
+			doctype:"Sidebar Header",
+            fields:["*"]
+		},
+		callback: function(r) {
+            if(r.message){
+                sidebarHeader = r.message.sort((a,b) => (a.urutan > b.urutan) ? 1 : ((b.urutan > a.urutan) ? -1 : 0));
+                console.log("Sidebar Header", sidebarHeader);
+                funcGetSidebarItem();
+            }
+        }
+    });
+
+    // Get sidebar menu
+    var sidebarMenu = [];
+    var funcGetSidebarItem = ()=>{
+        frappe.call({
+            method:"bdtheme.provider.get_resource_complete",
+            args: {
+                parent_table:"tabSidebar Menu",
+                child_tables:'[["submenu", "tabSidebar Menu Items"]]'
+            },
+            callback: function(r) {
+                if(r.message){
+                    sidebarMenu = r.message.sort((a,b) => (a.urutan > b.urutan) ? 1 : ((b.urutan > a.urutan) ? -1 : 0));
+                    console.log("Sidebar Menu", sidebarMenu);
+                    funcGenerate();
+                }
+            }
+        });
+    }
+
+    
+    var funcGenerate = ()=>{
+        // Add sidebar container
+        var ret = $.parseHTML('\
+        <aside class="main-sidebar">\
+            <section class="sidebar">\
+                <ul class="sidebar-menu">\
+                </ul>\
+            </section>\
+        </aside>\
+        <div class="overlay">\
+            <div>\
+        ');
+        var menu = $(ret).find('.sidebar-menu');
+
+        // Add sidebar header&item from doctype
+        sidebarHeader.forEach(el => {
+            menu.append('<li class="header">' + el.title + '</li>');
+
+            sidebarMenu.filter(m => m.header == el.title).forEach(el=>{
+                var li = $.parseHTML('\
+                    <li class="treeview">\
+                        <a href="#">\
+                            <i class="'+el.icon+'"></i>\
+                            <span>'+el.title+'</span>\
+                            <span class="pull-right-container">\
+                                <i class="fa fa-angle-left pull-right"></i>\
+                            </span>\
+                        </a>\
+                        <ul class="treeview-menu"></ul>\
+                    </li>\
+                ');
+
+
+                el.submenu.forEach(el=>{
+                    $(li).find('.treeview-menu').append('\
+                        <li><a href="'+el.href+'"><i class="'+el.icon+'"></i> '+el.title+'</a></li>\
+                    ')
+                });
+
+                menu.append(li);
+            });
+        });
+        
+
+        // Add default menu item
+        menu.append('\
+            <li class="header">Other</li>\
+            <li>\
+                <a href="#modules/Setup"><i class="fa fa-cog"></i> <span>Setup</span></a>\
+            </li>\
+        ')
+
+        callback(ret);
+    }
+}
 
 bdtheme.set_user_background = function(src, selector, style){
 	if(!selector) selector = "#page-desktop";
